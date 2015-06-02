a4d1840b56439baae8e54521590fc8d12f3ee487
vinchi777 <jude_calimbas@yahoo.com>
2015-06-03 03:02:15 +0800

    enhance design

--- assets/js/post/PostController.js
+++ assets/js/post/PostController.js
@@ -2,22 +2,24 @@ app.controller('PostCtrl', ['$scope', '$http', function($scope, $http){
   $scope.post = {};
   $scope.posts = [];
   $scope.postId = "";
-  $scope.load = false;
+  $scope.loading = true;
   $scope.editorOptions = {
     lineWrapping : true,
     lineNumbers: true
   };
 
   $scope.createPost = function(load){
-    $scope.load = true;
+    $scope.loading = true;
     $http.post('/post', $scope.formData)
       .success(function(data){
 	if (load == 'load_posts') getPosts();
 	if (load == 'load_post') getPost();
 	$scope.formData = {};
+
+	$scope.postform.$setPristine()
       })
       .catch(function(err){ console.log(err); })
-      .finally(function(){ $scope.load = false; });
+      .finally(function(){ $scope.loading = false; });
   }
 
   $scope.$watch('postId', function(){
@@ -37,7 +39,10 @@ app.controller('PostCtrl', ['$scope', '$http', function($scope, $http){
       .success(function(data){
 	$scope.posts = data;
       })
-    .catch(function(){ console.log("error"); });
+    .catch(function(){ console.log("error"); })
+    .finally(function(){
+      $scope.loading = false;
+    });
   }
 
   getPosts()
